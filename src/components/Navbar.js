8d201d9019f18a7a06098c18e57a1500f75bcb37
jeni <jenireecastro@gmail.com>
2023-04-18 22:30:04 -0500

    changed navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
+    <>
     <nav class="navbar navbar-expand-lg d-flex">
       <div class="container-fluid">
         <button
@@ -12,13 +13,13 @@ const Navbar = () => {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
-        >
+          >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div
           class="collapse navbar-collapse justify-content-center"
           id="navbarNav"
-        >
+          >
           <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="/">
@@ -32,28 +33,33 @@ const Navbar = () => {
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#aprende">
-                Aprende con nosotros
+                Aprendamos Juntos
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#about">
-                Conócenos
+                Conóceme
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">
+                Contáctame
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#s">
-                Contáctanos
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#s">
-                Aula Virtual{" "}
+                Aula Virtual
               </a>
             </li>
           </ul>
+        <div class="d-grid gap-2 d-md-flex justify-content-end">
+  <button class="btn btn-light  me-md-2 " type="button">Inicia sesión</button>
+  <button class="btn btn-light " type="button">Regístrate </button>
         </div>
       </div>
+</div>
     </nav>
+          </>
   );
 };
 export default Navbar;
